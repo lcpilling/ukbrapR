375b599eaeedd5c5df8b0513d66aa862a745e684
Luke C. Pilling <luke.pilling@gmail.com>
2023-11-14 15:04:05 +0000

    Update get_emr_diagnoses.R

--- R/get_emr_diagnoses.R
+++ R/get_emr_diagnoses.R
@@ -1,30 +1,40 @@
-#' Use R on the UK Biobank RAP (Spark cluster) to get phenotype data
+#' Get UK Biobank participant medical records data 
 #'
-#' @description Using a Spark node/cluster on the UK Biobank RAP, run R to extract a provided set of variables. Using code from the UK Biobank DNAnexus team https://github.com/UK-Biobank/UKB-RAP-Notebooks/blob/main/NBs_Prelim/105_export_participant_data_to_r.ipynb
+#' @description Using a Spark node/cluster on the UK Biobank Research Analysis Platform (DNAnexus), use R to get medical records for specific diagnostic codes list
 #'
-#' @return Returns a data.frame (the participant data for the requested variables)
+#' @return Returns a list of data frames (the participant data for the requested diagnosis codes: `death_cause`, `hesin_diag`, and `gp_clinical`)
 #'
 #' @author Luke Pilling
 #'
-#' @name get_rap_phenos
+#' @name get_emr_diagnoses
 #'
-#' @param names A string or vector of strings. The variable name(s) required. e.g., c("eid","p31","p21003_i0") (character)
-#' @param record A string. The `dnanexus_link` file descriptor of the .dataset to use. Default (if left as NULL) is to use the most recent update (character)
+#' @param codes_df A data frame. Contains the `vocab_col` and `codes_col` i.e., a list of diagnostic codes, and an indicator of the vocabulary.
+#' @param vocab_col A string. Column name in `codes_df` that contains the vocabulary indicator for the code (vocab IDs should be 'ICD10' 'Read2' or 'CTV3').
+#'        \code{default='vocab_id'}
+#' @param codes_col A string. Column name in `codes_df` that contains the diagnostic code ('ICD10' 'Read2' or 'CTV3' codes).
+#'        \code{default='code'}
+#' @param spark_master A string. The `master` argmuent passed to `sparklyr::spark_connect()`.
+#'        \code{default='spark://master:41000'}
 #' @param verbose Logical. Be verbose,
 #'        \code{default=FALSE}
 #'
 #' @examples
 #' # example diagnostic codes for CKD from GEMINI multimorbidity project
 #' codes_df <- readr::read_tsv("https://raw.githubusercontent.com/GEMINI-multimorbidity/diagnostic_codes/main/codelists/CKD.txt")
+#' codes_df
 #'
 #' # get diagnosis data - returns list of data frames (one per source)
-#' diagnoses_list <- get_emr_diagnoses(codes_df, verbose=TRUE)
-#' 
-#' # save to file on the RAP worker node
-#' write_tsv(df, "ukb14631.data_output.20231114.txt.gz")
+#' diagnoses_list <- ukbrapR::get_emr_diagnoses(codes_df)
+#' # 7 ICD10 codes, 40 Read2 codes, 37 CTV3 codes 
+#' # 298.18 sec elapsed
+#'
+#' # save to files on the RAP worker node
+#' readr::write_tsv(diagnoses_list$death_cause, "ukb14631.CKD.death_cause.20231114.txt.gz")
+#' readr::write_tsv(diagnoses_list$hesin_diag,  "ukb14631.CKD.hesin_diag.20231114.txt.gz")
+#' readr::write_tsv(diagnoses_list$gp_clinical, "ukb14631.CKD.gp_clinical.20231114.txt.gz")
 #' 
 #' # upload data to RAP storage
-#' ukbrapR::upload_to_rap(file="ukb14631.data_output.20231114.txt.gz", dir="")
+#' ukbrapR::upload_to_rap(file="ukb14631.CKD.*.20231114.txt.gz", dir="")
 #'
 #' @export
 #'
@@ -34,25 +44,29 @@ get_emr_diagnoses <- function(codes_df,
                               spark_master = "spark://master:41000",
                               verbose=FALSE)  {
 	
+	start_time <- Sys.time()
+
 	# Check input
-  if (verbose) cat("Check inputs\n")
-  if (! any(class(codes_df) %in% c("data.frame","tbl_df")))  stop("Codelist needs to be provided as a data frame")
-  codes_df = as.data.frame(codes_df)  # in case a tibble
-  
-  if (! vocab_col %in% colnames(codes_df))  stop("Codelist data frame needs to include vocabulary column. Specify with `vocab_col`")
-  if (! codes_col %in% colnames(codes_df))  stop("Codelist data frame needs to include vocabulary column. Specify with `vocab_col`")
-  
-  if (! any(c("ICD10","Read2","CTV3") %in% codes_df[,vocab_col]))  stop("Vocabularies need to include ICD10, Read2 or CTV3")
-  
-  
+	if (verbose) cat("Check inputs\n")
+	if (! any(class(codes_df) %in% c("data.frame","tbl_df")))  stop("Codelist needs to be provided as a data frame")
+	codes_df = as.data.frame(codes_df)  # in case a tibble
+
+	if (! vocab_col %in% colnames(codes_df))  stop("Codelist data frame needs to include vocabulary column. Specify with `vocab_col`")
+	if (! codes_col %in% colnames(codes_df))  stop("Codelist data frame needs to include vocabulary column. Specify with `vocab_col`")
+
+	if (! any(c("ICD10","Read2","CTV3") %in% codes_df[,vocab_col]))  stop("Vocabularies need to include ICD10, Read2 or CTV3")
+
+
 	# Check code lists - only first 5 digits are used by UK Biobank
 	if (verbose) cat("Check code lists - only first 5 digits are used by UK Biobank\n")
-		get_icd10 = FALSE
-		get_read2 = FALSE
-		get_ctv3  = FALSE
-		ICD10s    = ""
-		Read2s    = ""
-		CTV3s     = ""
+	get_icd10 = FALSE
+	get_read2 = FALSE
+	get_ctv3  = FALSE
+	ICD10s    = ""
+	Read2s    = ""
+	CTV3s     = ""
+	
+	# get ICD10s. Remove "." dot characters. First 5 characters only. Add "%" suffix to each for SQL LIKE query
 	if (any(codes_df[,vocab_col] == "ICD10"))  {
 		get_icd10 = TRUE
 		ICD10s    = codes_df |>
@@ -63,19 +77,21 @@ get_emr_diagnoses <- function(codes_df,
 			stringr::str_remove(stringr::fixed(".")) |> 
 			stringr::str_sub(1, 5) |> 
 			stringr::str_c("%")
-		cat("Will search `hesin` and `death_cause` for", length(ICD10s), "unique ICD10 codes\n")
+		cat(" - N unique ICD10 codes:", length(ICD10s), "\n")
 	}
+
+	# get Read2 and CTV3s. First 5 characters only. 
 	if (any(codes_df[,vocab_col] == "Read2"))  {
 		get_read2 = TRUE
 		Read2s    = codes_df[ codes_df[,vocab_col] == "Read2" , codes_col ]
 		Read2s    = stringr::str_sub(Read2s, 1, 5) |> unique()
-		cat("Will search `gp_clinical` for", length(Read2s), "unique Read2 codes\n")
+		cat(" - N unique Read2 codes:", length(Read2s), "\n")
 	}
 	if (any(codes_df[,vocab_col] == "CTV3"))  {
 		get_ctv3 = TRUE
 		CTV3s    = codes_df[ codes_df[,vocab_col] == "CTV3" , codes_col ]
 		CTV3s    = stringr::str_sub(CTV3s, 1, 5) |> unique()
-		cat("Will search `gp_clinical` for", length(CTV3s), "unique CTV3 codes\n")
+		cat(" - N unique CTV3 codes:", length(CTV3s), "\n")
 	}
 
 	#
@@ -105,7 +121,7 @@ get_emr_diagnoses <- function(codes_df,
 
 		# because we want to also match child codes for e.g., chapter codes we will use SQL `LIKE` 
 		# need to make the query manually because only fixed patterns are supported on database backends
-		if (verbose) cat(" - `death_cause`\n")
+		if (verbose) cat(" - death_cause\n")
 		death_cause_query = stringr::str_c(
 			"SELECT * FROM death_cause WHERE cause_icd10 LIKE '",
 			stringr::str_flatten(ICD10s, collapse = "' OR cause_icd10 LIKE '"),
@@ -114,7 +130,7 @@ get_emr_diagnoses <- function(codes_df,
 		death_cause_tbl = dplyr::tbl(sc, dbplyr::sql(death_cause_query))
 
 		# same for hesin_diag, but will also join with the episode date info etc.
-		if (verbose) cat(" - `hesin_diag`\n")
+		if (verbose) cat(" - hesin_diag\n")
 		hesin_diag_query = stringr::str_c(
 			"SELECT hesin_diag.eid, hesin_diag.diag_ICD10, hesin_diag.dnx_hesin_id, hesin.epistart, hesin.epiend, hesin.admidate, hesin.disdate ",
 			"FROM hesin_diag ",
@@ -127,22 +143,25 @@ get_emr_diagnoses <- function(codes_df,
 		
 	}
 	if (get_read2 | get_ctv3)  {
-		if (verbose) cat(" - `gp_clinical`\n")
+		if (verbose) cat(" - gp_clinical\n")
 		gp_clinical_tbl = dplyr::tbl(sc, "gp_clinical")
 		gp_clinical_tbl = gp_clinical_tbl |> dplyr::filter(read_2 %in% !!Read2s | read_3 %in% !!CTV3s) 
 	}
 	
 	# Collect data to memory
 	if (verbose) cat("Collect data to memory\n")
+	death_cause = NA
+	hesin_diag  = NA
+	gp_clinical = NA
 	if (get_icd10)  {
-		if (verbose) cat("- death_cause\n")
+		if (verbose) cat(" - death_cause\n")
 		death_cause = sparklyr::collect(death_cause_tbl)
 
-		if (verbose) cat("- hesin\n")
+		if (verbose) cat(" - hesin\n")
 		hesin_diag = sparklyr::collect(hesin_diag_tbl)
 	}
 	if (get_read2 | get_ctv3)  {
-		if (verbose) cat("- gp_clinical\n")
+		if (verbose) cat(" - gp_clinical\n")
 		gp_clinical = sparklyr::collect(gp_clinical_tbl)
 	}
 	
@@ -150,8 +169,11 @@ get_emr_diagnoses <- function(codes_df,
 	#
 	#
 	
+	end_time <- Sys.time()
+	if (verbose)  cat("Done.\nTime taken:", end_time - start_time, "\n")
+	
 	# Return data as list
-	list(death_cause, hesin, gp_clinical)
+	list(death_cause=death_cause, hesin_diag=hesin_diag, gp_clinical=gp_clinical)
 
 }
 
