4318adb46b9706c392be60d64d3da6d99c5d5972
Luke C. Pilling <luke.pilling@gmail.com>
2024-08-23 13:23:42 +0100

    Update internal paths
    For `indy` and `snow` (not the RAP)

--- data-raw/internal_files.R
+++ data-raw/internal_files.R
@@ -28,32 +28,56 @@ ukbrapr_paths = data.frame(
 		"ukbrapr_data/baseline_dates.tsv"
 	)
 )
+
 indy_paths = data.frame(
-	object=c("hesin","hesin_diag","hesin_oper","death","death_cause","gp_clinical","gp_scripts","selfrep_illness","cancer_registry"),
+	object=c(
+		"hesin",
+		"hesin_diag",
+		"hesin_oper",
+		"gp_clinical",
+		"gp_scripts",
+		"death",
+		"death_cause",
+		"selfrep_illness",
+		"cancer_registry",
+		"baseline_dates"),
 	path=c(
 		"/indy/ukbiobank/data_14631/hes/2022/_download/hesin.txt",
 		"/indy/ukbiobank/data_14631/hes/2022/_download/hesin_diag.txt",
 		"/indy/ukbiobank/data_14631/hes/2022/_download/hesin_oper.txt",
-		"/indy/ukbiobank/data_14631/mortality/ukb14631_death_20230412.txt",
-		"/indy/ukbiobank/data_14631/mortality/ukb14631_death_cause_20230412.txt",
 		"/indy/ukbiobank/data_14631/gp/2017/raw/gp_clinical.txt",
 		"/indy/ukbiobank/data_14631/gp/2017/raw/gp_scripts.txt",
-		"/indy/ukbiobank/data_14631/derived_phenotypes/diagnoses/ukb14631.selfrep_phenos.txt.gz",
-		"/indy/ukbiobank/data_14631/derived_phenotypes/diagnoses/ukb14631.cancer_registry.txt.gz"
+		"/indy/ukbiobank/data_14631/mortality/ukb14631_death_20230412.txt",
+		"/indy/ukbiobank/data_14631/mortality/ukb14631_death_cause_20230412.txt",
+		"/indy/ukbiobank/data_14631/derived_phenotypes/diagnoses/selfrep_illness.tsv",
+		"/indy/ukbiobank/data_14631/derived_phenotypes/diagnoses/cancer_registry.tsv",
+		"/indy/ukbiobank/data_14631/downloads/20211019_update_all_fields/baseline_dates.tsv"
 	)
 )
+
 snow_paths = data.frame(
-	object=c("hesin","hesin_diag","hesin_oper","death","death_cause","gp_clinical","gp_scripts","selfrep_illness","cancer_registry"),
+	object=c(
+		"hesin",
+		"hesin_diag",
+		"hesin_oper",
+		"gp_clinical",
+		"gp_scripts",
+		"death",
+		"death_cause",
+		"selfrep_illness",
+		"cancer_registry",
+		"baseline_dates"),
 	path=c(
 		"H:/Projects/BioBank/14631_ageing-well/HES up to 2022 Oct/_download/hesin.txt",
 		"H:/Projects/BioBank/14631_ageing-well/HES up to 2022 Oct/_download/hesin_diag.txt",
 		"H:/Projects/BioBank/14631_ageing-well/HES up to 2022 Oct/_download/hesin_oper.txt",
-		"H:/Projects/BioBank/14631_ageing-well/Death data/ukb14631_death_20230412.txt",
-		"H:/Projects/BioBank/14631_ageing-well/Death data/ukb14631_death_cause_20230412.txt",
 		"H:/Projects/BioBank/14631_ageing-well/GP up to 2017 June/_download/gp_clinical.txt",
 		"H:/Projects/BioBank/14631_ageing-well/GP up to 2017 June/_download/gp_scripts.txt",
-		"H:/Projects/BioBank/14631_ageing-well/diagnoses/ukb14631.selfrep_phenos.txt.gz",
-		"H:/Projects/BioBank/14631_ageing-well/diagnoses/ukb14631.cancer_registry.txt.gz"
+		"H:/Projects/BioBank/14631_ageing-well/Death data/ukb14631_death_20230412.txt",
+		"H:/Projects/BioBank/14631_ageing-well/Death data/ukb14631_death_cause_20230412.txt",
+		"H:/Projects/BioBank/14631_ageing-well/diagnoses/selfrep_illness.tsv",
+		"H:/Projects/BioBank/14631_ageing-well/diagnoses/cancer_registry.tsv",
+		"H:/Projects/BioBank/14631_ageing-well/data downloaded 2021 October - update all fields/baseline_dates.tsv"
 	)
 )
 
