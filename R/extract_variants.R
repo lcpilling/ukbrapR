445bc7f1ece83f8a965701c01fba0578e52b5d2b
Luke C. Pilling <luke.pilling@gmail.com>
2024-11-01 14:57:45 +0000

    Start outlining functions
    1. Get BED file
    2. Load and return as DF
    3. Create polygenic score

---
+++ R/extract_variants.R
@@ -0,0 +1,19 @@
+
+
+
+# make one BED file for provided variants
+
+file_plink  <- system.file("files", "plink_linux_x86_64_20240818.zip", package="ukbrapR")
+file_dragen <- system.file("files", "dragen_pvcf_coordinates.csv.gz", package="ukbrapR")
+
+
+
+# load BED file and return as data frame
+
+
+# create polygenic score of variants in BED file
+
+
+
+
+
