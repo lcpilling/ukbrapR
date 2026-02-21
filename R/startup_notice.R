f9e3dac89b6bfc3c79e9ce58f2c6b7ace55809be
Luke C. Pilling <luke.pilling@gmail.com>
2026-02-21 23:10:31 +0000

    Update startup_notice.R

--- R/startup_notice.R
+++ R/startup_notice.R
@@ -9,9 +9,9 @@
   if (!isTRUE(getOption("ukbrapR.feedback_notice_shown", FALSE))) {
     options(ukbrapR.feedback_notice_shown = TRUE)
 
-    cli::cli_ul(c(
-      "Thanks for using {.pkg ukbrapR}. I am keen to understand how widely it is being used.",
-      "Please star the repo {cli::symbol$star} and consider sharing feedback {.url https://github.com/lcpilling/ukbrapR}"
+    cli::cli_inform(c(
+      " " = "Hi! Once-per-session message from Luke. I am keen to understand how widely {.pkg ukbrapR} is used. Please {.emph star} the repo ({cli::symbol$star} {.url https://github.com/lcpilling/ukbrapR}), {.strong cite it in papers}, and consider sharing feedback. Thanks for supporting {.pkg ukbrapR}!",
+      ""
     ))
   }
 
