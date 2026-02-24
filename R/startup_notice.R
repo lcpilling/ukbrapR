1c246e65f58860557eab720aeb581e2cf1dfd30f
Luke C. Pilling <luke.pilling@gmail.com>
2026-02-24 16:35:20 +0000

    Add space to startup notice
    So there is a clear gap between notification and the "usual" output

--- R/startup_notice.R
+++ R/startup_notice.R
@@ -10,7 +10,8 @@
     options(ukbrapR.feedback_notice_shown = TRUE)
 
     cli::cli_inform(c(
-      " " = "Hi! Once-per-session message from Luke. I am keen to understand how widely {.pkg ukbrapR} is used. Please consider {.emph starring} the repo ({cli::symbol$star} {.url https://github.com/lcpilling/ukbrapR}), {.strong citing it in papers}, and sharing feedback. Thanks for supporting {.pkg ukbrapR}!",
+      "",
+      "i" = "Hi! Once-per-session message from Luke. I am keen to understand how widely {.pkg ukbrapR} is used. Please consider {.emph starring} the repo ({cli::symbol$star} {.url https://github.com/lcpilling/ukbrapR}), {.strong citing it in papers}, and sharing feedback. Thanks for supporting {.pkg ukbrapR}!",
       ""
     ))
   }
